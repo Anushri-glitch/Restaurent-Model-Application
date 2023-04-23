293b1d753e33a0d3b917a602ef2100236835d747
Anuhshka Srivastava <shrishtisrivastava19@gmail.com>
2023-04-23 22:31:28 +0530

    New Restaurent Commit

--- src/main/java/com/Shrishti/RestaurantModelApplication/Controller/RestroController.java
+++ src/main/java/com/Shrishti/RestaurantModelApplication/Controller/RestroController.java
@@ -2,35 +2,37 @@ package com.Shrishti.RestaurantModelApplication.Controller;
 
 import com.Shrishti.RestaurantModelApplication.Model.Restaurent;
 import com.Shrishti.RestaurantModelApplication.Service.RestroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/resto-app")
+@RequestMapping("/api/restro-app")
 public class RestroController {
-    private final RestroService restoService;
+    @Autowired
+    RestroService restoService;
     public RestroController(RestroService restoService){
         this.restoService=restoService;
     }
-    @PostMapping("/add-restaurant")
+    @PostMapping("/add-restaurent")
     public void addResto(@RequestBody Restaurent restaurantModelClass){
         restoService.addRestaurant(restaurantModelClass);
     }
-    //http://localhost:8080/api/resto-app/find-restaurant/name/{name}
-    //http://localhost:8080/api/resto-app/find-restaurant/name/SagarRatna
-    @GetMapping("/find-restaurant/name/{name}")
+    //http://localhost:8080/api/restro-app/find-restaurant/name/{name}
+    //http://localhost:8080/api/restro-app/find-restaurant/name/SagarRatna
+    @GetMapping("/find-restaurent/name/{name}")
     public Restaurent findRestaurantByName(@PathVariable String name){
         return restoService.findByName(name);
     }
-    //http://localhost:8080/api/resto-app/findall-restaurant
-    @GetMapping("/findall-restaurant")
+    //http://localhost:8080/api/restro-app/findall-restaurent
+    @GetMapping("/findall-restaurent")
     public List<Restaurent> findAllRestos(){
         return restoService.findAllRestaurant();
     }
-    //http://localhost:8080/api/resto-app/update-restaurant/number/{number}
+    //http://localhost:8080/api/resto-app/update-restaurent/number/{number}
     /*
-    http://localhost:8080/api/resto-app/update-restaurant/number/1001
+    http://localhost:8080/api/restro-app/update-restaurent/number/1001
     JSON FORMAT-:
      {
         "resto_name": "Sagar Ratna",
@@ -40,13 +42,13 @@ public class RestroController {
         "resto_totalstaff": 200
     }
      */
-    @PutMapping("/update-restaurant/number/{number}")
+    @PutMapping("/update-restaurent/number/{number}")
     public void updateRestaurant(@PathVariable int number,@RequestBody Restaurent restaurantModelClass){
         restoService.updateRestaurant(number,restaurantModelClass);
     }
-    //http://localhost:8080/api/resto-app/delete-restaurant/number/{number}
-    //http://localhost:8080/api/resto-app/delete-restaurant/number/1001
-    @DeleteMapping("/delete-restaurant/number/{number}")
+    //http://localhost:8080/api/resto-app/delete-restaurent/number/{number}
+    //http://localhost:8080/api/resto-app/delete-restaurent/number/1001
+    @DeleteMapping("/delete-restaurent/number/{number}")
     public void deleteRestaurant(@PathVariable int number){
         restoService.deleteRestaurant(number);
     }
